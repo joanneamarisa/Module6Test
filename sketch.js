e8d4066241e28f5588cc4951fd3c4010d46e2ea8
joanneamarisa <66515877+joanneamarisa@users.noreply.github.com>
2023-09-27 12:29:06 +1000

    Upload sketch files

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var canvasDiv;
+
+function setup() {
+  canvasDiv = document.getElementById("mySketch");
+  
+  let canvas = createCanvas(canvasDiv.offsetWidth, windowHeight/2);
+  canvas.parent("mySketch");
+  background(200, 200, 100, 20);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(random(255), random(255), random(255));
+  } else {
+     fill(255);
+  }
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    circle(mouseX, mouseY, 30);
+}
+
+function windowResized() {
+  resizeCanvas(canvasDiv.offsetWidth, windowHeight/2);
+}
